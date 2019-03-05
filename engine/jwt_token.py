11de797015bb76750429d184fb4a3f543687d77b
Eviatar Gerzi <evyatar575@gmail.com>
2019-03-05 12:18:02 +0200

    Fix bug with decoding token with 0x82 byte (issue #3)
    Based on stackoverflow:
    https://stackoverflow.com/questions/12468179/unicodedecodeerror-utf8-codec-cant-decode-byte-0x9c
    There are two common suggestions:
    1. Ignoring the error by running: `decoded_data.decode("utf-8", errors='ignore')` but this will omit the '0x82' byte from the token.
    2. Use 'latin-1' decoding
    
    We will use the second option to provide the user with all the characters.

--- engine/jwt_token.py
+++ engine/jwt_token.py
@@ -3,8 +3,16 @@ import base64
 
 def decode_jwt_token_data(jwt_token):
     splitted_string = jwt_token.split(".")
-    decoded_data = decode_base64(splitted_string[1])
-    return decoded_data.decode("utf-8")
+    decoded_data_base64 = decode_base64(splitted_string[1])
+    decoded_data = ''
+    try:
+        decoded_data = decoded_data_base64.decode("utf-8")
+    except Exception as e:
+        print('[*] An error occured while trying to deocde the JWT token:')
+        print(str(e))
+        print("[*] Decoding the token with latin-1 instead of UTF-8...")
+        decoded_data = decoded_data_base64.decode("latin-1")
+    return decoded_data
 
 def decode_base64(data):
     """Decode base64, padding being optional.
