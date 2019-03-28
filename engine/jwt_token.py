9cb20d8dd596640889e4f22ec2b87ae73b1ceeb1
Eviatar Gerzi <evyatar575@gmail.com>
2019-03-28 12:53:25 +0200

    Added function to decode base64 jwt token (#7)

--- engine/jwt_token.py
+++ engine/jwt_token.py
@@ -1,9 +1,15 @@
 
 import base64
 
+def decode_base64_jwt_token(base64_token):
+    return decode_jwt_token_data(decode_base64_bytes_to_string(decode_base64(base64_token)))
+
 def decode_jwt_token_data(jwt_token):
     splitted_string = jwt_token.split(".")
     decoded_data_base64 = decode_base64(splitted_string[1])
+    return decode_base64_bytes_to_string(decoded_data_base64)
+
+def decode_base64_bytes_to_string(decoded_data_base64):
     decoded_data = ''
     try:
         decoded_data = decoded_data_base64.decode("utf-8")
@@ -13,6 +19,7 @@ def decode_jwt_token_data(jwt_token):
         print("[*] Decoding the token with latin-1 instead of UTF-8...")
         decoded_data = decoded_data_base64.decode("latin-1")
     return decoded_data
+
 
 def decode_base64(data):
     """Decode base64, padding being optional.
