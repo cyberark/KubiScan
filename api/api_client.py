ad55e1cfa59c56fcee4c355e0fd0cf51b3c67f7b
Eviatar Gerzi <evyatar575@gmail.com>
2019-07-23 14:43:39 +0300

    Fix checking if inside a docker container
    On CentOs when running a container, the `proc/self/cgroup` have rows with the following format:  
    `11:perf_event:/system.slice/docker-70988aff535e4d29172ed917b27a321a3074aad6d1edd844fb434ad5874a3c2e.scope`  
    
    Therefore it didn't identify it as a container.
    Added another check that will identify it.

--- api/api_client.py
+++ api/api_client.py
@@ -23,7 +23,7 @@ def running_in_docker_container():
     with open('/proc/self/cgroup', 'r') as procfile:
         for line in procfile:
             fields = line.strip().split('/')
-            if 'docker' in fields:
+            if 'docker' in fields or '/docker-' in line:
                 return True
     return False
 
