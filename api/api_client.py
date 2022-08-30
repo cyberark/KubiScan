64a5c5e6eea6ad5b15f810f1daf432eddce68fff
Natan <natan.tunik@cyberark.com>
2022-08-30 15:01:26 +0300

    Changing the function 'running_in_docker_container' to 'running_in_container'. Not the function checks if running in a general container, no necessarily docker container.

--- api/api_client.py
+++ api/api_client.py
@@ -23,11 +23,11 @@ RbacAuthorizationV1Api = None
 
 def running_in_container():
     with open('/proc/1/comm') as file:
-        line = file.readline()
+        line = file.readline().strip()
         while line:
             if 'systemd' in line:
                 return False
-            line = file.readline()
+            line = file.readline().strip()
     return True
 def replace(file_path, pattern, subst):
     #Create temp file
