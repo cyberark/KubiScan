5add4f1c9447ba1b59c2bbf615e331845a01e826
regevnoam1 <regevnoam1@gmail.com>
2024-09-16 17:34:38 +0300

    unit_test_change

--- unit_test.py
+++ unit_test.py
@@ -6,7 +6,6 @@ from .KubiScan import get_all_affecting_cves_table_by_version
 import json
 from api.config import set_api_client
 from api.api_client import api_init, running_in_container
-from api.client_factory import ApiClientFactory
 from api.config import set_api_client
 
 list_of_risky_containers = ["test1-yes", "test3-yes", "test5ac2-yes", "test6a-yes", "test6b-yes",
@@ -64,8 +63,7 @@ def get_all_cve_from_json():
 
 
 class TestKubiScan(unittest.TestCase):
-    api_client = ApiClientFactory.get_client(use_static=False)
-    api_client.api_init()
+    api_client = api_client.api_init()
     set_api_client(api_client)
 
     def test_get_risky_pods(self):
